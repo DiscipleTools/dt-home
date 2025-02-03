0798a908c8330f0187e5e4308048e0d05bdd1fcc
Muniraj <muni20002raj@gmail.com>
2025-02-03 19:47:18 +0530

    Refactor LocalAnalytics service to keep log instead of counts

--- src/Services/AnalyticsReporting.php
+++ src/Services/AnalyticsReporting.php
@@ -27,10 +27,9 @@ class AnalyticsReporting
      * @param array $params The parameters
      * @return array The analytics reports
      */
-    public function get_reports( array $params = [] ): array
+    public function get_reports( array $params = [], $start_date = null, $end_date = null ): array
     {
         $reports = [];
-
         $dynamic_metrics = [
             'total-active-apps-count' => 'get_active_apps_count',
             'total-active-custom-apps-count' => 'get_active_custom_apps_count',
@@ -45,12 +44,11 @@ class AnalyticsReporting
             }
         }
 
-        $stored_metrics = get_plugin_option( config( 'analytics.dt_home_analytics_reports' ), [] );
         $db_metrics = [ 'admin-app-creation', 'user-app-creation', 'login', 'login-error', 'logout' ];
 
         foreach ( $db_metrics as $metric ) {
             if ( in_array( $metric, $params ) ) {
-                $reports[$metric] = $stored_metrics[$metric] ?? 0;
+                $reports['activity-log'][$metric] = $this->get_activity_logs( $metric, $start_date, $end_date );
             }
         }
 
@@ -122,14 +120,74 @@ class AnalyticsReporting
     }
 
     /**
+     * Initialize the database
+     *
+     * @return array The database connection, table name, and user ID
+     */
+    private function initialize_db(): array
+    {
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'dt_activity_log';
+        $userId = get_current_user_id();
+
+        return [ $wpdb, $table_name, $userId ];
+    }
+
+    /**
      * Log an admin event
      *
      * @param string $eventName The event name
      */
     public function log_admin_event( string $eventName ): void
     {
-        $stored_metrics = get_plugin_option( config( 'analytics.dt_home_analytics_reports' ), [] );
-        $stored_metrics[$eventName] = ( $stored_metrics[$eventName] ?? 0 ) + 1;
-        set_plugin_option( config( 'analytics.dt_home_analytics_reports' ), $stored_metrics );
+        [$wpdb, $table_name, $userId] = $this->initialize_db();
+
+        $wpdb->insert(
+            $table_name,
+            [
+                'user_id' => $userId,
+                'action' => $eventName,
+                'hist_time' => time(),
+                'object_type' => 'dt_home_event',
+                'object_name' => $eventName,
+            ],
+            [
+                '%d',
+                '%s',
+                '%d',
+                '%s',
+                '%s',
+                '%s'
+            ]
+        );
+    }
+
+    /**
+     * Get the activity logs
+     *
+     * @param string $eventName The event name
+     * @return array The activity logs
+     */
+    private function get_activity_logs( string $eventName, $start_date = null, $end_date = null ): array
+    {
+        [$wpdb, $table_name] = $this->initialize_db();
+
+        $query = "SELECT histid, user_id, action, object_type, object_name, FROM_UNIXTIME(hist_time) as hist_time FROM $table_name WHERE object_type = %s";
+        $params = [ 'dt_home_event' ];
+
+        if ( $eventName ) {
+            $query .= " AND action = %s";
+            $params[] = $eventName;
+        }
+
+        if ( $start_date && $end_date ) {
+            $query .= " AND hist_time BETWEEN %s AND %s";
+            $params[] = strtotime( $start_date );
+            $params[] = strtotime( $end_date );
+        }
+
+        $results = $wpdb->get_results( $wpdb->prepare( $query, ...$params ), ARRAY_A );
+
+        return $results;
     }
 }
