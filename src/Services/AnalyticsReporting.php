8e2683f35182826004b682b55dcec1fab031f6bb
Muniraj <muni20002raj@gmail.com>
2025-02-04 18:40:23 +0530

    remove the dynamic calculations

--- src/Services/AnalyticsReporting.php
+++ src/Services/AnalyticsReporting.php
@@ -2,25 +2,8 @@
 
 namespace DT\Home\Services;
 
-use DT\Home\Sources\Trainings;
-use function DT\Home\get_plugin_option;
-use function DT\Home\set_plugin_option;
-use DT\Home\Sources\SettingsApps;
-use function DT\Home\config;
-
 class AnalyticsReporting
 {
-    private SettingsApps $settings_apps;
-    private Trainings $trainings_source;
-    private array $apps = [];
-
-    public function __construct( SettingsApps $source, Trainings $trainings_source )
-    {
-        $this->settings_apps = $source;
-        $this->trainings_source = $trainings_source;
-        $this->apps = $this->settings_apps->undeleted();
-    }
-
     /**
      * Get the analytics reports
      *
@@ -30,93 +13,27 @@ class AnalyticsReporting
     public function get_reports( array $params = [], $start_date = null, $end_date = null ): array
     {
         $reports = [];
-        $dynamic_metrics = [
-            'total-active-apps-count' => 'get_active_apps_count',
-            'total-active-custom-apps-count' => 'get_active_custom_apps_count',
-            'total-active-coded-apps-count' => 'get_active_coded_apps_count',
-            'total-deleted-coded-apps-count' => 'get_deleted_coded_apps_count',
-            'total-active-training-videos-count' => 'get_active_training_videos_count'
+
+        $db_metrics = [
+            'admin-app-creation',
+            'user-app-creation',
+            'login',
+            'login-error',
+            'logout',
+            'total-active-apps-count',
+            'total-active-custom-apps-count',
+            'total-active-coded-apps-count',
+            'total-deleted-coded-apps-count',
+            'total-active-training-videos-count'
         ];
-
-        foreach ( $dynamic_metrics as $param => $method ) {
-            if ( in_array( $param, $params ) ) {
-                $reports[$param] = $this->$method();
-            }
-        }
-
-        $db_metrics = [ 'admin-app-creation', 'user-app-creation', 'login', 'login-error', 'logout' ];
 
         foreach ( $db_metrics as $metric ) {
             if ( in_array( $metric, $params ) ) {
-                $reports['activity-log'][$metric] = $this->get_activity_logs( $metric, $start_date, $end_date );
+                $reports[$metric] = $this->get_activity_logs( $metric, $start_date, $end_date );
             }
         }
 
         return $reports;
-    }
-
-    /**
-     * Get the count of active apps
-     *
-     * @return int The count of active apps
-     */
-    private function get_active_apps_count(): int
-    {
-        return $this->count_apps_by_filter( fn( $app ) => !$app['is_hidden'] );
-    }
-
-    /**
-     * Get the count of active custom apps
-     *
-     * @return int The count of active custom apps
-     */
-    private function get_active_custom_apps_count(): int
-    {
-        return $this->count_apps_by_filter( fn( $app ) => !$app['is_hidden'] && $app['creation_type'] === 'custom' );
-    }
-
-    /**
-     * Get the count of active coded apps
-     *
-     * @return int The count of active coded apps
-     */
-    private function get_active_coded_apps_count(): int
-    {
-        return $this->count_apps_by_filter( fn( $app ) => !$app['is_hidden'] && $app['creation_type'] === 'code' );
-    }
-
-    /**
-     * Get the count of deleted coded apps
-     *
-     * @return int The count of deleted coded apps
-     */
-
-    private function get_deleted_coded_apps_count(): int
-    {
-        $deleted_coded_apps = $this->settings_apps->deleted();
-
-        return count( array_filter( $deleted_coded_apps, fn( $app ) => $app['creation_type'] === 'code' ) );
-    }
-
-    /**
-     * Get the count of active training videos
-     *
-     * @return int The count of active training videos
-     */
-    private function get_active_training_videos_count(): int
-    {
-        return count( $this->trainings_source->all() );
-    }
-
-    /**
-     * Count the number of apps that match a filter
-     *
-     * @param callable $filter The filter to apply
-     * @return int The number of apps that match the filter
-     */
-    private function count_apps_by_filter( callable $filter ): int
-    {
-        return count( array_filter( $this->apps, $filter ) );
     }
 
     /**
