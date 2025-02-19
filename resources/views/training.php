3b493cfedbb92ec171391dcf10af650cc7681e01
Perumal.R <75656133+Perumal605@users.noreply.github.com>
2025-02-19 20:04:55 +0530

    Link the hamburger menu apps link directly to the magic link URL

--- resources/views/training.php
+++ resources/views/training.php
@@ -3,14 +3,15 @@
 $this->layout( 'layouts/plugin' );
 /**
  * @var string $data
+ * @var string $key
  */
 ?>
 <h1 class="training"> <?php esc_html_e( 'Training', 'dt-home' ); ?></h1>
 <dt-tile>
-    <dt-home-video-list training-data='<?php echo htmlspecialchars( $data ); // phpcs:ignore ?>'></dt-home-video-list>
+    <dt-home-video-list training-data='<?php echo htmlspecialchars($data); // phpcs:ignore ?>'></dt-home-video-list>
 </dt-tile>
 
 <?php
-$this->insert( 'partials/return-to-launcher-button' );
+$this->insert( 'partials/return-to-launcher-button', [ 'key' => $key ] );
 ?>
 
