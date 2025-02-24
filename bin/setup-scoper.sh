7d162b1db142ed3dd9bb1f464402209f670117f6
Craig Wann <craig@codezone.io>
2025-02-24 15:14:10 -0600

    Loosing scoper requirement.

--- bin/setup-scoper.sh
+++ bin/setup-scoper.sh
@@ -2,7 +2,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 if ! command -v php-scoper > /dev/null 2>&1; then
   echo "PHP Scoper is not installed. Installing..."
-
+  composer global config --no-plugins allow-plugins.wpify/scoper true
   composer global require humbug/php-scoper -W
   composer global require wpify/scoper -W
 fi
