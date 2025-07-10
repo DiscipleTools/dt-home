07156dcd24958dd458f8c22de5fb71e57dce38af
corsacca <corsacca@gmail.com>
2025-07-10 13:53:59 +0100

    Implement manifest controller for PWA support. Add to homescreen now works

---
+++ src/Controllers/MagicLink/ManifestController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace DT\Home\Controllers\MagicLink;
+
+use DT\Home\Psr\Http\Message\ResponseInterface;
+use DT\Home\Psr\Http\Message\ServerRequestInterface as Request;
+use function DT\Home\response;
+
+class ManifestController {
+    public function show( Request $request, $params ): ResponseInterface {
+        $key = $params['key'];
+        $launcher_url = '/apps/launcher/' . $key;
+
+        $manifest = [
+            'name' => 'D.T Home',
+            'short_name' => 'D.T Home',
+            'description' => 'D.T HomeSreen',
+            'start_url' => $launcher_url,
+            'display' => 'standalone',
+            'background_color' => '#ffffff',
+            'theme_color' => '#3f729b',
+            'icons' => [
+                [
+                    'src' => get_template_directory_uri() . '/dt-assets/favicons/android-chrome-192x192.png',
+                    'sizes' => '192x192',
+                    'type' => 'image/png',
+                ],
+                [
+                    'src' => get_template_directory_uri() . '/dt-assets/favicons/android-chrome-512x512.png',
+                    'sizes' => '512x512',
+                    'type' => 'image/png',
+                ],
+                [
+                    'src' => get_template_directory_uri() . '/dt-assets/favicons/apple-touch-icon.png',
+                    'sizes' => '180x180',
+                    'type' => 'image/png',
+                ],
+            ],
+        ];
+
+        $json_manifest = json_encode( $manifest, JSON_UNESCAPED_SLASHES );
+
+        return response( $json_manifest )
+            ->withHeader( 'Content-Type', 'application/json' );
+    }
+} 
--- src/Services/Template.php
+++ src/Services/Template.php
@@ -5,6 +5,7 @@ namespace DT\Home\Services;
 use DT\Home\CodeZone\WPSupport\Router\ResponseRendererInterface;
 use DT\Home\Psr\Http\Message\ResponseInterface;
 use function DT\Home\namespace_string;
+use function DT\Home\request;
 
 /**
  * Class Template
@@ -30,6 +31,7 @@ class Template implements ResponseRendererInterface
 	 * @return void
 	 */
 	public function register() {
+		add_filter( 'dt_override_header_meta', '__return_true' );
 		add_filter( 'dt_blank_access', [ $this, 'blank_access' ] );
 		add_action( 'dt_blank_head', [ $this, 'header' ] );
 		add_action( 'dt_blank_footer', [ $this, 'footer' ] );
@@ -51,6 +53,39 @@ class Template implements ResponseRendererInterface
 	 * Render the header
 	 */
 	public function header() {
+		$path = request()->getUri()->getPath();
+		if ( preg_match( '#^/apps/launcher/([^/]+)#', $path, $matches ) ) {
+			$key              = $matches[1];
+			$manifest_url     = '/apps/launcher/' . $key . '/manifest.json';
+			$app_title        = 'D.T Home';
+			$theme_color      = '#3f729b';
+			$background_color = '#ffffff';
+			$favicon_path     = get_template_directory_uri() . '/dt-assets/favicons';
+
+			// Manifest
+			echo '<link rel="manifest" href="' . esc_url( $manifest_url ) . '">';
+
+			// Apple PWA
+			echo '<meta name="apple-mobile-web-app-capable" content="yes">';
+			echo '<meta name="mobile-web-app-capable" content="yes">';
+			echo '<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">';
+			echo '<meta name="apple-mobile-web-app-title" content="' . esc_attr( $app_title ) . '">';
+			echo '<link rel="apple-touch-icon" sizes="180x180" href="' . esc_url( $favicon_path ) . '/apple-touch-icon.png">';
+
+			// Standard Favicons
+			echo '<link rel="icon" type="image/png" sizes="32x32" href="' . esc_url( $favicon_path ) . '/favicon-32x32.png">';
+			echo '<link rel="icon" type="image/png" sizes="16x16" href="' . esc_url( $favicon_path ) . '/favicon-16x16.png">';
+			echo '<link rel="shortcut icon" href="' . esc_url( $favicon_path ) . '/favicon.ico">';
+
+			// Microsoft
+			echo '<meta name="msapplication-TileColor" content="' . esc_attr( $theme_color ) . '">';
+			echo '<meta name="msapplication-TileImage" content="' . esc_url( $favicon_path ) . '/mstile-144x144.png">';
+			echo '<meta name="msapplication-config" content="' . esc_url( $favicon_path ) . '/browserconfig.xml">';
+
+			// Theme
+			echo '<meta name="theme-color" content="' . esc_attr( $theme_color ) . '">';
+			echo '<meta name="background-color" content="' . esc_attr( $background_color ) . '">';
+		}
 		wp_head();
 	}
 
