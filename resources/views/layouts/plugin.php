b1ed81dd47df9f232ecf77192d797bf9d1ba31c9
kodinkat <19330800+kodinkat@users.noreply.github.com>
2025-07-15 07:02:59 +0100

    Add invite modal functionality and translations for multiple languages (#318)

--- resources/views/layouts/plugin.php
+++ resources/views/layouts/plugin.php
@@ -10,12 +10,21 @@ $menu_items = [];
 // Adding default menu items
 $menu_items[] = [ 'label' => __( 'Apps', 'dt-home' ), 'href' => $home ];
 $menu_items[] = [ 'label' => __( 'Training', 'dt-home' ), 'href' => magic_url( 'training' ) ];
+$menu_items[] = [ 'label' => __( 'Invite', 'dt-home' ), 'href' => '#', 'action' => 'invite' ];
 //if ( get_option( 'dt_home_require_login', true ) ) {
 $menu_items[] = [ 'label' => __( 'Log Out', 'dt-home' ), 'href' => magic_url( 'logout' ) ];
 //}
 
 
 $menu_items_json = wp_json_encode( $menu_items );
+$invite_translations = [
+    'inviteTitle' => __( 'Invite', 'dt-home' ),
+    'inviteExplanation' => __( 'Copy this link and share it with people you are coaching. They will create their own account and have their own Home Screen.', 'dt-home' ),
+    'inviteCopyButton' => __( 'Copy', 'dt-home' ),
+    'inviteCloseButton' => __( 'Close', 'dt-home' ),
+    'inviteCopiedMessage' => __( 'Link copied!', 'dt-home' ),
+];
+$invite_translations_json = wp_json_encode( $invite_translations );
 ?>
 
 <sp-theme
@@ -26,7 +35,7 @@ $menu_items_json = wp_json_encode( $menu_items );
         <div class="plugin__main">
             <div class="container non-selectable">
 
-                <dt-home-menu menuItems='<?php echo esc_js( $menu_items_json ); ?>'></dt-home-menu>
+                <dt-home-menu menuItems='<?php echo esc_js( $menu_items_json ); ?>' translations='<?php echo esc_js( $invite_translations_json ); ?>'></dt-home-menu>
 
                 <?php
                 // phpcs:ignore
