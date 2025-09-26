643c96fb87204276cd9396a5f6ffa8380cc8cc83
corsacca <corsacca@gmail.com>
2025-09-26 15:11:12 +0100

    v1.2.0

--- dt-home.php
+++ dt-home.php
@@ -5,7 +5,7 @@
  * Description: An app home screen for disciple.tools. Part of the DT Toolbox.
  * Text Domain: dt-home
  * Domain Path: /languages
- * Version:  1.1.1
+ * Version:  1.2.0
  * Author URI: https://github.com/DiscipleTools
  * GitHub Plugin URI: https://github.com/DiscipleTools/dt-home
  * Requires at least: 4.7.0
